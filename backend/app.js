990a9f5e11f81f73795b3abf9d04bed5fd82a29e
tzharkih <tzharkih@telematika.com>
2021-11-13 16:19:25 +0300

    add backend

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.post('/api/posts', (req, res, next) => {
+  const post = req.body;
+  res.status(201).json({
+    message: 'Все окей'
+  });
+})
+
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {id: 1, title: 'title', content: 'content'},
+    {id: 2, title: 'title', content: 'content'},
+    {id: 3, title: 'title', content: 'content'},
+  ];
+  res.status(200) .json(posts);
+})
+
+module.exports = app;
